ffe8bf2e6f3791d516fd25b43acf9460b2bbb62b
almdoug <douglassurf2014@gmail.com>
2025-01-08 16:54:08 -0300

    mvp

--- src/database/conection.php
+++ src/database/conection.php
@@ -1,11 +1,12 @@
 <?php
-    if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
-    if (!defined('DB_NAME')) define('DB_NAME', 'facilestoque');
-    if (!defined('DB_USER')) define('DB_USER', 'root');
-    if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
+    if (!defined('DB_HOST')) define('DB_HOST', $_ENV['DB_HOST']);
+    if (!defined('DB_NAME')) define('DB_NAME', $_ENV['DB_NAME']);
+    if (!defined('DB_USER')) define('DB_USER', $_ENV['DB_USER']);
+    if (!defined('DB_PASSWORD')) define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+    if (!defined('DB_PORT')) define('DB_PORT', $_ENV['DB_PORT']);
 
     try {
-        $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4", DB_USER, DB_PASSWORD);
+        $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4;port=".DB_PORT, DB_USER, DB_PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     } catch(PDOException $e) {
